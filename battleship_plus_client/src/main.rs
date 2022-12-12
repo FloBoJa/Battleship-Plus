8413ddeba217beded4cf48be14d35b294a985437
Florian Bossert <florian.bossert@student.kit.edu>
2022-12-12 10:04:20 +0100

    Divide into sub-packages and add basic client

---
+++ battleship_plus_client/src/main.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(2.0, 4.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn spawn_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh = meshes.add(
+        shape::Icosphere {
+            radius: 1.0,
+            subdivisions: 4,
+        }
+        .into(),
+    );
+    let material = materials.add(Color::rgb(0.6, 0.8, 0.4).into());
+    commands.spawn(PbrBundle {
+        mesh,
+        material,
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 5.0),
+        ..Default::default()
+    });
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_scene)
+        .run();
+}
