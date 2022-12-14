85746f399d6565f12b6dc6bc9c31afa4909a6ce9
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2022-12-14 23:22:45 +0100

    create ShipID and PlayerID types

--- crates/battleship_plus_server/src/game/actions.rs
+++ crates/battleship_plus_server/src/game/actions.rs
@@ -1,21 +1,23 @@
 use battleship_plus_common::messages::{EngineBoostRequest, MoveRequest, MultiMissileRequest, PredatorMissileRequest, RotateRequest, ScoutPlaneRequest, SetPlacementRequest, SetReadyStateRequest, ShootRequest, TorpedoRequest};
 
+use crate::game::data::{PlayerID, ShipID};
+
 #[derive(Debug, Clone)]
 pub enum Action {
     // Lobby actions
-    TeamSwitch { player_id: u32 },
-    SetReady { player_id: u32, request: SetReadyStateRequest },
+    TeamSwitch { player_id: PlayerID },
+    SetReady { player_id: PlayerID, request: SetReadyStateRequest },
 
     // Preparation actions
-    PlaceShips { player_id: u32, request: SetPlacementRequest },
+    PlaceShips { player_id: PlayerID, request: SetPlacementRequest },
 
     // Game actions
-    Move { player_id: u32, ship_id: u32, request: MoveRequest },
-    Rotate { player_id: u32, ship_id: u32, request: RotateRequest },
-    Shoot { player_id: u32, ship_id: u32, request: ShootRequest },
-    ScoutPlane { player_id: u32, ship_id: u32, request: ScoutPlaneRequest },
-    PredatorMissile { player_id: u32, ship_id: u32, request: PredatorMissileRequest },
-    EngineBoost { player_id: u32, ship_id: u32, request: EngineBoostRequest },
-    Torpedo { player_id: u32, ship_id: u32, request: TorpedoRequest },
-    MultiMissile { player_id: u32, ship_id: u32, request: MultiMissileRequest },
+    Move { player_id: PlayerID, ship_id: ShipID, request: MoveRequest },
+    Rotate { player_id: PlayerID, ship_id: ShipID, request: RotateRequest },
+    Shoot { player_id: PlayerID, ship_id: ShipID, request: ShootRequest },
+    ScoutPlane { player_id: PlayerID, ship_id: ShipID, request: ScoutPlaneRequest },
+    PredatorMissile { player_id: PlayerID, ship_id: ShipID, request: PredatorMissileRequest },
+    EngineBoost { player_id: PlayerID, ship_id: ShipID, request: EngineBoostRequest },
+    Torpedo { player_id: PlayerID, ship_id: ShipID, request: TorpedoRequest },
+    MultiMissile { player_id: PlayerID, ship_id: ShipID, request: MultiMissileRequest },
 }
