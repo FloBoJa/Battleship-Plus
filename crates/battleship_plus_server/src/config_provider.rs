a66e930ffaee9d0944f920e583d4d5b0aa232d7c
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2022-12-14 16:22:04 +0100

    extend config provider: server config

--- crates/battleship_plus_server/src/config_provider.rs
+++ crates/battleship_plus_server/src/config_provider.rs
@@ -1,11 +1,31 @@
-trait ConfigProvider {
-    fn get_config(&self) -> battleship_plus_common::messages::Config;
+use std::net::{SocketAddrV4, SocketAddrV6};
+use std::sync::Arc;
+use std::time::Duration;
+
+#[derive(Copy, Clone, Debug)]
+pub struct ServerConfig {
+    pub game_address_v4: SocketAddrV4,
+    pub game_address_v6: SocketAddrV6,
+    pub enable_announcements_v4: bool,
+    pub enable_announcements_v6: bool,
+    pub announcement_address_v4: SocketAddrV4,
+    pub announcement_address_v6: SocketAddrV6,
+    pub announcement_interval: Duration,
+}
+
+pub trait ConfigProvider {
+    fn game_config(&self) -> Arc<battleship_plus_common::messages::Config>;
+    fn server_config(&self) -> Arc<ServerConfig>;
 }
 
 mod default {
+    use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
+    use std::sync::Arc;
+    use std::time::Duration;
+
     use battleship_plus_common::messages::{BattleshipBalancing, CarrierBalancing, CommonBalancing, Config, Costs, CruiserBalancing, DestroyerBalancing, ShipType, SubmarineBalancing, TeamShipSet};
 
-    use crate::config_provider::ConfigProvider;
+    use crate::config_provider::{ConfigProvider, ServerConfig};
 
     fn costs(cooldown: i32, action_points: i32) -> Option<Costs> {
         Some(Costs {
@@ -33,11 +53,12 @@ mod default {
         })
     }
 
+    #[derive(Copy, Clone, Debug)]
     pub struct DefaultGameConfig;
 
     impl ConfigProvider for DefaultGameConfig {
-        fn get_config(&self) -> Config {
-            Config {
+        fn game_config(&self) -> Arc<Config> {
+            Arc::from(Config {
                 server_name: String::from("Battleship PLUS powered by Rust 🦀"),
                 carrier_balancing: Some(CarrierBalancing {
                     common_balancing: Some(CommonBalancing {
@@ -110,11 +131,23 @@ mod default {
                 action_point_gain: 1,
                 team_size_a: 2,
                 team_size_b: 2,
-            }
+            })
+        }
+
+        fn server_config(&self) -> Arc<ServerConfig> {
+            Arc::from(ServerConfig {
+                game_address_v4: SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 30303),
+                game_address_v6: SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 30303, 0, 0),
+                enable_announcements_v4: true,
+                enable_announcements_v6: true,
+                announcement_address_v4: SocketAddrV4::new(Ipv4Addr::BROADCAST, 30304),
+                announcement_address_v6: SocketAddrV6::new(Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 1), 30304, 0, 0),
+                announcement_interval: Duration::from_secs(5),
+            })
         }
     }
 }
 
-pub fn default_config_provider() -> Box<dyn ConfigProvider> {
-    Box::new(default::DefaultGameConfig {})
+pub fn default_config_provider() -> Arc<dyn ConfigProvider> {
+    Arc::from(default::DefaultGameConfig)
 }
