23296229c5bc268c605fd0a477822e22a840c24b
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2022-12-16 00:46:32 +0100

    add shoot action tests

--- crates/battleship_plus_server/src/game/actions_test.rs
+++ crates/battleship_plus_server/src/game/actions_test.rs
@@ -156,3 +156,189 @@ mod actions_player_set_ready_state {
     }
 }
 
+mod actions_shoot {
+    use std::collections::{HashMap, HashSet};
+    use std::sync::Arc;
+
+    use rstar::RTree;
+    use tokio::sync::RwLock;
+
+    use battleship_plus_common::messages::{CommonBalancing, Coordinate, Costs, DestroyerBalancing, ShootRequest};
+
+    use crate::game::actions::{Action, ActionExecutionError};
+    use crate::game::data::{Game, GetShipID, Player, Ship, ShipData, ShipRef};
+
+    #[tokio::test]
+    async fn actions_shoot() {
+        let player = Player::default();
+        let ship_src = Ship::Destroyer {
+            balancing: Arc::from(DestroyerBalancing {
+                common_balancing: Some(CommonBalancing {
+                    shoot_damage: 10,
+                    shoot_range: 128,
+                    shoot_costs: Some(Costs { cooldown: 0, action_points: 0 }),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            data: Default::default(),
+            cool_downs: Default::default(),
+        };
+        let ship_target1 = Ship::Destroyer {
+            balancing: Arc::from(DestroyerBalancing {
+                common_balancing: Some(CommonBalancing {
+                    ..ship_src.common_balancing()
+                }),
+                ..Default::default()
+            }),
+            data: ShipData {
+                id: (2, 2),
+                player_id: 2,
+                health: 10,
+                pos_x: 5,
+                pos_y: 5,
+                ..ship_src.data()
+            },
+            cool_downs: Default::default(),
+        };
+        let ship_target2 = Ship::Destroyer {
+            balancing: Arc::from(DestroyerBalancing {
+                common_balancing: Some(CommonBalancing {
+                    ..ship_src.common_balancing()
+                }),
+                ..Default::default()
+            }),
+            data: ShipData {
+                id: (2, 3),
+                player_id: 2,
+                health: 11,
+                pos_x: 10,
+                pos_y: 10,
+                ..ship_src.data()
+            },
+            cool_downs: Default::default(),
+        };
+
+        let game = Arc::new(RwLock::new(
+            Game {
+                board_size: 24,
+                players: HashMap::from([
+                    (player.id, player.clone())
+                ]),
+                team_a: HashSet::from([player.id]),
+                ships: HashMap::from([
+                    (ship_src.id(), ship_src.clone()),
+                    (ship_target1.id(), ship_target1.clone()),
+                    (ship_target2.id(), ship_target2.clone()),
+                ]),
+                ships_geo_lookup: RTree::bulk_load(vec![
+                    ShipRef(Arc::from(ship_src)),
+                    ShipRef(Arc::from(ship_target1.clone())),
+                    ShipRef(Arc::from(ship_target2.clone())),
+                ]),
+                ..Default::default()
+            }
+        ));
+
+
+        // shoot ship_target1
+        assert!(Action::Shoot {
+            player_id: player.id,
+            request: ShootRequest {
+                ship_number: 0,
+                target: Some(Coordinate { x: ship_target1.data().pos_x as u32, y: ship_target1.data().pos_y as u32 }),
+            },
+        }.apply_on(game.clone()).await.is_ok());
+
+        // check ship_target1 destroyed and ship_target2 untouched
+        {
+            let g = game.read().await;
+            assert_eq!(g.ships.contains_key(&ship_target1.id()), false);
+            assert_eq!(g.ships.contains_key(&ship_target2.id()), true);
+            assert_eq!(g.ships.get(&ship_target2.id()).unwrap().data().health, 11);
+        }
+
+        // shoot ship_target2
+        assert!(Action::Shoot {
+            player_id: player.id,
+            request: ShootRequest {
+                ship_number: 0,
+                target: Some(Coordinate { x: ship_target2.data().pos_x as u32, y: ship_target2.data().pos_y as u32 }),
+            },
+        }.apply_on(game.clone()).await.is_ok());
+
+        // check ship_target1 destroyed and ship_target2 health reduced
+        {
+            let g = game.read().await;
+            assert_eq!(g.ships.contains_key(&ship_target1.id()), false);
+            assert_eq!(g.ships.contains_key(&ship_target2.id()), true);
+            assert_eq!(g.ships.get(&ship_target2.id()).unwrap().data().health, 1);
+        }
+
+        // missed shot
+        assert!(Action::Shoot {
+            player_id: player.id,
+            request: ShootRequest {
+                ship_number: 0,
+                target: Some(Coordinate { x: 20, y: 20 }),
+            },
+        }.apply_on(game.clone()).await.is_ok());
+
+        // board untouched
+        {
+            let g = game.read().await;
+            assert_eq!(g.ships.contains_key(&ship_target1.id()), false);
+            assert_eq!(g.ships.contains_key(&ship_target2.id()), true);
+            assert_eq!(g.ships.get(&ship_target2.id()).unwrap().data().health, 1);
+        }
+    }
+
+    #[tokio::test]
+    async fn actions_shoot_unknown_player() {
+        let game = Arc::new(RwLock::new(
+            Game {
+                ..Default::default()
+            }
+        ));
+
+        let res = Action::Shoot {
+            player_id: 42,
+            request: ShootRequest {
+                ship_number: 1,
+                target: Some(Coordinate { x: 0, y: 0 }),
+            },
+        }.apply_on(game.clone()).await;
+
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert!(match err {
+            ActionExecutionError::Illegal(e) => e == "PlayerID 42 is unknown",
+            _ => false
+        })
+    }
+
+    #[tokio::test]
+    async fn actions_shoot_reject_shot_into_oblivion() {
+        let game = Arc::new(RwLock::new(
+            Game {
+                board_size: 24,
+                ..Default::default()
+            }
+        ));
+
+        let res = Action::Shoot {
+            player_id: 42,
+            request: ShootRequest {
+                ship_number: 1,
+                target: Some(Coordinate { x: 9999, y: 9999 }),
+            },
+        }.apply_on(game.clone()).await;
+
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert!(match err {
+            ActionExecutionError::Illegal(e) => e == "PlayerID 42 is unknown",
+            _ => false
+        })
+    }
+}
