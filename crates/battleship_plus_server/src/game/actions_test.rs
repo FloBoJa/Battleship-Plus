9aec3d0988dccde554b51c9ecb80e3a8fc31c3b6
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2022-12-15 14:09:00 +0100

    add tests for team switch action

--- crates/battleship_plus_server/src/game/actions_test.rs
+++ crates/battleship_plus_server/src/game/actions_test.rs
@@ -1,47 +1,94 @@
-use std::collections::{HashMap, HashSet};
-use std::sync::Arc;
+mod actions_team_switch {
+    use std::collections::{HashMap, HashSet};
+    use std::sync::Arc;
 
-use tokio::sync::RwLock;
+    use tokio::sync::RwLock;
 
-use crate::game::actions::Action;
-use crate::game::data::{Game, Player, PlayerID};
+    use crate::game::actions::{Action, ActionExecutionError};
+    use crate::game::data::{Game, Player, PlayerID};
 
-#[tokio::test]
-async fn actions_team_switch() {
-    let player_id: PlayerID = 42;
-    let game = Arc::new(RwLock::new(
-        Game {
-            players: HashMap::from([
-                (player_id, Player {
-                    id: player_id,
-                    ..Default::default()
-                })
-            ]),
-            team_a: HashSet::from([player_id]),
-            ..Default::default()
+    #[tokio::test]
+    async fn actions_team_switch() {
+        let player_id: PlayerID = 42;
+        let game = Arc::new(RwLock::new(
+            Game {
+                players: HashMap::from([
+                    (player_id, Player {
+                        id: player_id,
+                        ..Default::default()
+                    })
+                ]),
+                team_a: HashSet::from([player_id]),
+                ..Default::default()
+            }
+        ));
+
+        // player is in team a
+        {
+            let g = game.read().await;
+            assert!(g.team_a.contains(&player_id));
+            assert!(!g.team_b.contains(&player_id));
         }
-    ));
 
-    // player is in team a
-    {
-        let g = game.read().await;
-        assert!(g.team_a.contains(&player_id));
-        assert!(!g.team_b.contains(&player_id));
+        // switch team a -> b
+        assert!(Action::TeamSwitch { player_id }.apply_on(game.clone()).await.is_ok());
+        {
+            let g = game.read().await;
+            assert!(!g.team_a.contains(&player_id));
+            assert!(g.team_b.contains(&player_id));
+        }
+
+        // switch team b -> a
+        assert!(Action::TeamSwitch { player_id }.apply_on(game.clone()).await.is_ok());
+        {
+            let g = game.read().await;
+            assert!(g.team_a.contains(&player_id));
+            assert!(!g.team_b.contains(&player_id));
+        }
     }
 
-    // switch team a -> b
-    assert!(Action::TeamSwitch { player_id }.apply_on(game.clone()).await.is_ok());
-    {
-        let g = game.read().await;
-        assert!(!g.team_a.contains(&player_id));
-        assert!(g.team_b.contains(&player_id));
+    #[tokio::test]
+    async fn actions_team_switch_detect_inconsistent_state() {
+        let player_id: PlayerID = 42;
+        let game = Arc::new(RwLock::new(
+            Game {
+                players: HashMap::from([
+                    (player_id, Player {
+                        id: player_id,
+                        ..Default::default()
+                    })
+                ]),
+                team_a: HashSet::from([player_id]),
+                team_b: HashSet::from([player_id]),
+                ..Default::default()
+            }
+        ));
+
+        let res = Action::TeamSwitch { player_id }.apply_on(game.clone()).await;
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert!(match err {
+            ActionExecutionError::InconsistentState(e) => e == "found illegal team assignment for player 42",
+            _ => false
+        })
     }
 
-    // switch team b -> a
-    assert!(Action::TeamSwitch { player_id }.apply_on(game.clone()).await.is_ok());
-    {
-        let g = game.read().await;
-        assert!(g.team_a.contains(&player_id));
-        assert!(!g.team_b.contains(&player_id));
+    #[tokio::test]
+    async fn actions_team_switch_unknown_player() {
+        let player_id: PlayerID = 42;
+        let game = Arc::new(RwLock::new(
+            Game {
+                ..Default::default()
+            }
+        ));
+
+        let res = Action::TeamSwitch { player_id }.apply_on(game.clone()).await;
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert!(match err {
+            ActionExecutionError::Illegal(e) => e == "PlayerID 42 is unknown",
+            _ => false
+        })
     }
 }
+
