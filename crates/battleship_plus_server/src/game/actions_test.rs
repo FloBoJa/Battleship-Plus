a77f294e492e71ac199a162e9c3ce55479b1a281
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2022-12-15 14:59:58 +0100

    implement player set ready action + test

--- crates/battleship_plus_server/src/game/actions_test.rs
+++ crates/battleship_plus_server/src/game/actions_test.rs
@@ -92,3 +92,67 @@ mod actions_team_switch {
     }
 }
 
+mod actions_player_set_ready_state {
+    use std::collections::{HashMap, HashSet};
+    use std::sync::Arc;
+
+    use tokio::sync::RwLock;
+
+    use battleship_plus_common::messages::SetReadyStateRequest;
+
+    use crate::game::actions::{Action, ActionExecutionError};
+    use crate::game::data::{Game, Player, PlayerID};
+
+    #[tokio::test]
+    async fn actions_player_set_ready() {
+        let player_id: PlayerID = 42;
+        let game = Arc::new(RwLock::new(
+            Game {
+                players: HashMap::from([
+                    (player_id, Player {
+                        id: player_id,
+                        ..Default::default()
+                    })
+                ]),
+                team_a: HashSet::from([player_id]),
+                ..Default::default()
+            }
+        ));
+
+        // set player ready
+        assert!(Action::SetReady { player_id, request: SetReadyStateRequest { ready_state: true } }
+            .apply_on(game.clone()).await.is_ok());
+        {
+            let g = game.read().await;
+            assert_eq!(g.players.get(&player_id).unwrap().is_ready, true);
+        }
+
+        // set player not ready
+        assert!(Action::SetReady { player_id, request: SetReadyStateRequest { ready_state: false } }
+            .apply_on(game.clone()).await.is_ok());
+        {
+            let g = game.read().await;
+            assert_eq!(g.players.get(&player_id).unwrap().is_ready, false);
+        }
+    }
+
+    #[tokio::test]
+    async fn actions_set_ready_unknown_player() {
+        let player_id: PlayerID = 42;
+        let game = Arc::new(RwLock::new(
+            Game {
+                ..Default::default()
+            }
+        ));
+
+        let res = Action::SetReady { player_id, request: SetReadyStateRequest { ready_state: true } }
+            .apply_on(game.clone()).await;
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert!(match err {
+            ActionExecutionError::Illegal(e) => e == "PlayerID 42 is unknown",
+            _ => false
+        })
+    }
+}
+
