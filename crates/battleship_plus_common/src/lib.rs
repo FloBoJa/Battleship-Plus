9ee1af4cffc45576710bf7af1cf31c1c806ebd42
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2022-12-16 14:57:17 +0100

    rust format

--- crates/battleship_plus_common/src/lib.rs
+++ crates/battleship_plus_common/src/lib.rs
@@ -25,7 +25,9 @@ pub mod messages {
         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
             match self {
                 MessageEncodingError::IO(s) => f.write_str(format!("IO: {}", s).as_str()),
-                MessageEncodingError::PROTOCOL(s) => f.write_str(format!("PROTOCOL: {}", s).as_str()),
+                MessageEncodingError::PROTOCOL(s) => {
+                    f.write_str(format!("PROTOCOL: {}", s).as_str())
+                }
             }
         }
     }
@@ -39,24 +41,36 @@ pub mod messages {
     }
 
     impl Message {
-        pub fn version(&self) -> u8 { self.version }
-        pub fn op_code(&self) -> OpCode { self.op_code }
-        pub fn payload_length(&self) -> usize { self.payload.len() as usize }
-        pub fn payload(&self) -> &Vec<u8> { &self.payload }
+        pub fn version(&self) -> u8 {
+            self.version
+        }
+        pub fn op_code(&self) -> OpCode {
+            self.op_code
+        }
+        pub fn payload_length(&self) -> usize {
+            self.payload.len() as usize
+        }
+        pub fn payload(&self) -> &Vec<u8> {
+            &self.payload
+        }
 
         pub fn decode(rd: &mut dyn BufRead) -> Result<Message, MessageEncodingError> {
             let mut buf = [0u8; 4];
             match rd.read_exact(buf.borrow_mut()) {
                 Ok(_) => {}
-                Err(e) =>
-                    return Err(MessageEncodingError::IO(format!("unable to read header from buffer: {}", &e)))
+                Err(e) => {
+                    return Err(MessageEncodingError::IO(format!(
+                        "unable to read header from buffer: {}",
+                        &e
+                    )))
+                }
             }
 
             let version = buf[0];
 
             let op_code = match OpCode::try_from(buf[1]) {
                 Ok(op_code) => op_code,
-                Err(e) => return Err(MessageEncodingError::PROTOCOL(String::from(e)))
+                Err(e) => return Err(MessageEncodingError::PROTOCOL(String::from(e))),
             };
 
             let payload_length = (buf[2] as u16) << 8 | (buf[3] as u16);
@@ -64,8 +78,12 @@ pub mod messages {
 
             match rd.read_exact(payload.borrow_mut()) {
                 Ok(_) => {}
-                Err(e) =>
-                    return Err(MessageEncodingError::IO(format!("unable to read header from buffer: {}", e)))
+                Err(e) => {
+                    return Err(MessageEncodingError::IO(format!(
+                        "unable to read header from buffer: {}",
+                        e
+                    )))
+                }
             }
 
             Ok(Message {
@@ -75,9 +93,15 @@ pub mod messages {
             })
         }
 
-        pub fn new(version: u8, op_code: OpCode, payload: &[u8]) -> Result<Message, MessageEncodingError> {
+        pub fn new(
+            version: u8,
+            op_code: OpCode,
+            payload: &[u8],
+        ) -> Result<Message, MessageEncodingError> {
             if payload.len() > u16::MAX as usize {
-                return Err(MessageEncodingError::PROTOCOL(String::from("message payload is too long")));
+                return Err(MessageEncodingError::PROTOCOL(String::from(
+                    "message payload is too long",
+                )));
             }
 
             let payload = Vec::from(payload);
@@ -110,7 +134,10 @@ pub mod messages {
             let buf = self.encode();
             match writer.write_all(&buf) {
                 Ok(_) => Ok(()),
-                Err(e) => Err(MessageEncodingError::IO(format!("unable to write message: {}", e)))
+                Err(e) => Err(MessageEncodingError::IO(format!(
+                    "unable to write message: {}",
+                    e
+                ))),
             }
         }
     }
