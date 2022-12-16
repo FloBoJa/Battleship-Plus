5ad5f7f147ae0c4893125a125d10ced812d97963
Florian Bossert <florian.bossert@student.kit.edu>
2022-12-16 17:36:29 +0100

    Remove illegal type

--- crates/battleship_plus_client/src/networking.rs
+++ crates/battleship_plus_client/src/networking.rs
@@ -1,49 +1,35 @@
+use battleship_plus_common::types;
 use bevy::prelude::*;
 use bevy_quinnet::client::QuinnetClientPlugin;
-use battleship_plus_common::types;
 use std::net::Ipv6Addr;
 
 pub struct NetworkingPlugin;
 
 impl Plugin for NetworkingPlugin {
-  fn build(&self, app : &mut App) {
-    app.add_plugin(QuinnetClientPlugin{})
-        .add_startup_system(listen_for_announcements)
-        .add_system(needs_server)
-        .add_system(listen_for_lobby_change);
-  }
+    fn build(&self, app: &mut App) {
+        app.add_plugin(QuinnetClientPlugin {})
+            .add_startup_system(listen_for_announcements)
+            .add_system(needs_server);
+    }
 }
 
 #[derive(Component, Debug)]
 pub struct ServerInformation {
-  pub ip : std::net::IpAddr, pub port : u32, pub config : types::Config,
+    pub ip: std::net::IpAddr,
+    pub port: u32,
+    pub config: types::Config,
 }
 
 fn listen_for_announcements(mut commands: Commands) {
-  commands.spawn(ServerInformation{
-    ip : Ipv6Addr::new (0, 0, 0, 0, 0, 0, 0, 1).into(),
-    port : 30305,
-    config : types::Config{..default()},
-  });
+    commands.spawn(ServerInformation {
+        ip: Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1).into(),
+        port: 30305,
+        config: types::Config { ..default() },
+    });
 }
 
-fn needs_server(servers : Query<&ServerInformation>) {
-    for
-      server in servers.iter() { println !("{:?}", server); }
-}
-
-#[derive(Component, Debug)]
-pub struct LobbyState(types::LobbyTeamState, types::LobbyTeamState);
-
-fn listen_for_lobby_change(mut commands
-                           : Commands, lobby_state
-                           : Query<(Entity, &LobbyState)>) {
-  if
-    !lobby_state.is_empty() {
-      commands.get_entity(lobby_state.single() .0)
-          .expect("!!!!")
-          .despawn_recursive();
+fn needs_server(servers: Query<&ServerInformation>) {
+    for server in servers.iter() {
+        println!("{:?}", server);
     }
-  commands.spawn(LobbyState(..default(), ..default()));
-  return;
 }
