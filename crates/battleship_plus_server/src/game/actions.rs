189bc7420803c1948d8fc98115fd6df3c4571a5a
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2022-12-17 20:43:07 +0100

    hide operations on ship lookup structures in new structure

--- crates/battleship_plus_server/src/game/actions.rs
+++ crates/battleship_plus_server/src/game/actions.rs
@@ -1,14 +1,12 @@
 use std::sync::Arc;
 
 use log::{debug, error};
-use rstar::{Envelope, PointDistance, RTreeObject, AABB};
 use tokio::sync::RwLock;
 
 use battleship_plus_common::messages::*;
 
-use crate::game::data::{
-    Cooldown, Game, GetShipID, PlayerID, SelectShipsByIDFunction, Ship, ShipID, ShipRef,
-};
+use crate::game::data::{Game, PlayerID};
+use crate::game::ship::ShipID;
 use crate::game::states::GameState;
 
 #[derive(Debug, Clone)]
@@ -65,10 +63,21 @@ pub enum Action {
 
 #[derive(Debug, Clone)]
 pub enum ActionExecutionError {
+    Validation(ActionValidationError),
     OutOfState(GameState),
     Illegal(String),
     InconsistentState(String),
     ActionNotPossible(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum ActionValidationError {
+    NonExistentPlayer { id: PlayerID },
+    NonExistentShip { id: ShipID },
+    Cooldown { remaining_rounds: u32 },
+    InsufficientPoints { required: u32 },
+    Unreachable,
+    OutOfMap,
 }
 
 impl Action {
@@ -115,82 +124,24 @@ impl Action {
             // TODO: Action::PlaceShips { .. } => {}
             Action::Move { player_id, request } => {
                 check_player_exists(&game, *player_id).await?;
-
                 let ship_id = (*player_id, request.ship_number);
-                check_player_operates_ship(&game, ship_id).await?;
 
                 mutate_game(&game, |g| {
                     let board_bounds = g.board_bounds();
-                    let ship = g.ships.get_mut(&ship_id).unwrap();
+                    let player = g.players.get_mut(&player_id).unwrap();
 
-                    // move cooldown
-                    if ship
-                        .cool_downs()
-                        .iter()
-                        .any(|cd| matches!(cd, Cooldown::Movement { .. }))
-                    {
-                        return Err(ActionExecutionError::ActionNotPossible(format!(
-                            "the engines of ship {} of player {} is on cooldown",
-                            ship_id.1, player_id
-                        )));
-                    }
+                    let trajectory = match g.ships.move_ship(
+                        player,
+                        &ship_id,
+                        request.direction(),
+                        &board_bounds,
+                    ) {
+                        Ok(trajectory) => trajectory,
+                        Err(e) => return Err(ActionExecutionError::Validation(e)),
+                    };
 
-                    // action points
-                    let ship_balancing = ship.common_balancing();
-                    let point_costs = ship_balancing
-                        .movement_costs
-                        .as_ref()
-                        .unwrap()
-                        .action_points;
-                    let player = g.players.get_mut(player_id).unwrap();
-                    if point_costs > player.action_points {
-                        return Err(ActionExecutionError::ActionNotPossible(format!(
-                            "player {} needs {} action points for {:?} action but has only {}",
-                            player_id, &point_costs, self, player.action_points
-                        )));
-                    }
-
-                    let new_position = ship.do_move(request.direction(), board_bounds);
-                    if new_position.is_err() {
-                        return Err(ActionExecutionError::Illegal(format!(
-                            "player {} tried an invalid move with ship {}",
-                            player_id, ship_id.1,
-                        )));
-                    }
-                    let new_position = new_position.unwrap();
-
-                    g.ships_geo_lookup
-                        .remove_with_selection_function(SelectShipsByIDFunction(vec![(
-                            ship.id(),
-                            new_position,
-                        )]));
-
-                    let new_ship = ship.clone();
-                    g.ships_geo_lookup.insert(ShipRef(Arc::from(s)));
-                    g.ships.insert(ship_id, s);
-
-                    player.action_points -= point_costs;
-
-                    let colliding_ships: Vec<_> = g
-                        .ships_geo_lookup
-                        .locate_in_envelope_intersecting(&new_position)
-                        .cloned()
-                        .collect();
-
-                    if colliding_ships.len() > 1 {
-                        // remove colliding ships
-                        g.ships_geo_lookup
-                            .remove_with_selection_function(SelectShipsByIDFunction(
-                                colliding_ships
-                                    .iter()
-                                    .map(|s| (s.0.data().id, s.envelope()))
-                                    .collect(),
-                            ));
-
-                        colliding_ships.iter().for_each(|s| {
-                            g.ships.remove(&s.id());
-                        });
-                    }
+                    let _ /*destroyed_ships*/ = g.ships.destroy_colliding_ships_in_envelope(&trajectory);
+                    // TODO: find a way to propagate destroyed ships 
 
                     Ok(())
                 })
@@ -199,69 +150,27 @@ impl Action {
             // TODO: Action::Rotate { .. } => {}
             Action::Shoot { player_id, request } => {
                 check_player_exists(&game, *player_id).await?;
+                let ship_id = (*player_id, request.ship_number);
 
                 let target = [
                     request.target.as_ref().unwrap().x as i32,
                     request.target.as_ref().unwrap().y as i32,
                 ];
-                check_target_on_board(&game, &AABB::from_point(target)).await?;
 
-                let ship_id = (*player_id, request.ship_number);
-                check_player_operates_ship(&game, ship_id).await?;
+                match mutate_game(&game, |g| {
+                    let bounds = g.board_bounds();
+                    let player = g.players.get_mut(player_id).unwrap();
 
-                mutate_game(&game, |g| {
-                    let player = g.players.get(player_id).unwrap();
-                    let ship = g.ships.get(&ship_id).unwrap();
-                    let ship_balancing = ship.common_balancing();
-
-                    // shoot cooldown
-                    if ship
-                        .cool_downs()
-                        .iter()
-                        .any(|cd| matches!(cd, Cooldown::Cannon { .. }))
-                    {
-                        return Err(ActionExecutionError::ActionNotPossible(format!(
-                            "the cannon of ship {} of player {} is on cooldown",
-                            ship_id.1, player_id
-                        )));
-                    }
-
-                    // action points
-                    if ship_balancing.shoot_costs.as_ref().unwrap().action_points
-                        > player.action_points
-                    {
-                        return Err(ActionExecutionError::ActionNotPossible(format!(
-                            "player {} needs {} action points for {:?} action but has only {}",
-                            player_id,
-                            &ship_balancing.shoot_costs.unwrap().action_points,
-                            self,
-                            player.action_points
-                        )));
-                    }
-
-                    // target in range
-                    if ship.distance_2(&target) > ship_balancing.shoot_range as i32 {
-                        return Err(ActionExecutionError::ActionNotPossible(format!(
-                            "target is out of range for ship {}",
-                            ship_id.0
-                        )));
-                    }
-
-                    apply_damage(g, &AABB::from_point(target), ship_balancing.shoot_damage);
-
-                    let costs = ship_balancing.shoot_costs.unwrap();
-                    g.players.get_mut(player_id).unwrap().action_points -= costs.action_points;
-                    if costs.cooldown > 0 {
-                        g.ships.get_mut(&ship_id).unwrap().cool_downs_mut().push(
-                            Cooldown::Cannon {
-                                remaining_rounds: costs.cooldown,
-                            },
-                        );
-                    }
-
-                    Ok(())
+                    g.ships.attack_with_ship(player, &ship_id, &target, &bounds)
                 })
                 .await
+                {
+                    Ok(_) => {
+                        // TODO: find a way to propergate this result to the caller
+                        Ok(())
+                    }
+                    Err(e) => Err(ActionExecutionError::Validation(e)),
+                }
             }
             // TODO: Action::ScoutPlane { .. } => {}
             // TODO: Action::PredatorMissile { .. } => {}
@@ -273,46 +182,6 @@ impl Action {
 
         // TODO: find a good way to return Action Results
     }
-}
-
-/// Deals damage to all ship in the envelope and returns all destroyed ships
-fn apply_damage(game: &mut Game, target: &AABB<[i32; 2]>, damage: u32) -> Vec<Ship> {
-    let destroyed_ships: Vec<_> = game
-        .ships_geo_lookup
-        .locate_in_envelope_intersecting(target)
-        .filter(|ship_ref| {
-            let target_ship = game.ships.get_mut(&ship_ref.0.data().id).unwrap();
-            target_ship.apply_damage(damage)
-        })
-        .map(|s| (s.0.data().id, s.envelope()))
-        .collect();
-
-    game.ships_geo_lookup
-        .remove_with_selection_function(SelectShipsByIDFunction(destroyed_ships.clone()));
-
-    destroyed_ships
-        .iter()
-        .map(|(id, _)| game.ships.remove(id).unwrap())
-        .collect()
-}
-
-async fn check_target_on_board(
-    game: &Arc<RwLock<Game>>,
-    target: &AABB<[i32; 2]>,
-) -> Result<(), ActionExecutionError> {
-    read_game(game, |g| {
-        let board: &AABB<[i32; 2]> =
-            &AABB::from_corners([0, 0], [g.board_size as i32, g.board_size as i32]);
-
-        if !board.contains_envelope(target) {
-            let msg = "specified target out of bounds";
-            debug!("{}", msg);
-            Err(ActionExecutionError::Illegal(String::from(msg)))
-        } else {
-            Ok(())
-        }
-    })
-    .await
 }
 
 async fn check_player_exists(
@@ -327,24 +196,6 @@ async fn check_player_exists(
         } else {
             Ok(())
         }
-    })
-    .await
-}
-
-async fn check_player_operates_ship(
-    game: &Arc<RwLock<Game>>,
-    ship_id: ShipID,
-) -> Result<(), ActionExecutionError> {
-    read_game(game, |g| match g.ships.get(&ship_id) {
-        None => {
-            let msg = format!(
-                "Player {} does not control the ship {}",
-                ship_id.0, ship_id.1
-            );
-            debug!("{}", msg.as_str());
-            Err(ActionExecutionError::Illegal(msg))
-        }
-        Some(_) => Ok(()),
     })
     .await
 }
