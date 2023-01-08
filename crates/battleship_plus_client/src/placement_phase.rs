391384254d78c8aa0f6d76732df91c08ef65f9a3
Florian Bossert <florian.bossert@student.kit.edu>
2023-01-08 07:48:10 +0100

    Clean up

--- crates/battleship_plus_client/src/placement_phase.rs
+++ crates/battleship_plus_client/src/placement_phase.rs
@@ -4,20 +4,18 @@ use battleship_plus_common::types;
 
 #[derive(Resource)]
 pub struct Quadrant {
-    x: u32,
-    y: u32,
-    w: u32,
-    h: u32,
+    pub x: u32,
+    pub y: u32,
+    pub size: u32,
 }
 
 impl Quadrant {
-    pub fn new(top_left_corner: types::Coordinate, player_count: usize) -> Quadrant {
+    pub fn new(top_left_corner: types::Coordinate, _player_count: usize) -> Quadrant {
         // TODO: quadrant calculation
         Quadrant {
             x: top_left_corner.x,
             y: top_left_corner.y,
-            w: 1,
-            h: 1,
+            size: 1,
         }
     }
 }
