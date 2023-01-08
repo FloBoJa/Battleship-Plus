230a55dda8bc5662881355ec95388fc791fdb21b
Florian Bossert <florian.bossert@student.kit.edu>
2023-01-08 07:45:04 +0100

    Allow changing name

--- crates/battleship_plus_client/src/server_selection.rs
+++ crates/battleship_plus_client/src/server_selection.rs
@@ -8,6 +8,7 @@ use std::str::FromStr;
 use battleship_plus_common::messages::{self, StatusCode};
 
 use crate::game_state::{GameState, PlayerId};
+use crate::lobby;
 use crate::networking;
 
 pub struct ServerSelectionPlugin;
@@ -19,6 +20,7 @@ impl Plugin for ServerSelectionPlugin {
         }
         app.init_resource::<UiState>()
             .add_startup_system(setup_egui_font)
+            .add_startup_system(initialize_ui_state)
             .add_system(draw_selection_screen.run_in_state(GameState::Unconnected))
             .add_system(draw_joining_screen.run_in_state(GameState::Joining))
             .add_system(process_join_response.run_in_state(GameState::Joining))
@@ -32,6 +34,7 @@ struct UiState {
     server_address: String,
     error_message: String,
     connection_errored: bool,
+    user_name: String,
 }
 
 fn setup_egui_font(mut egui_context: ResMut<EguiContext>) {
@@ -59,6 +62,10 @@ fn setup_egui_font(mut egui_context: ResMut<EguiContext>) {
     egui_context.ctx_mut().set_fonts(fonts);
 }
 
+fn initialize_ui_state(mut ui_state: ResMut<UiState>, user_name: Res<lobby::UserName>) {
+    ui_state.user_name = user_name.to_string();
+}
+
 fn draw_selection_screen(
     mut commands: Commands,
     mut egui_context: ResMut<EguiContext>,
@@ -72,6 +79,9 @@ fn draw_selection_screen(
         ui.vertical_centered(|ui| {
             ui.set_max_width(750.0);
             ui.heading("Server Selection");
+
+            ui.add_space(20.0);
+
             TableBuilder::new(ui)
                 .striped(true)
                 .column(Column::at_least(Column::auto(), 250.0))
@@ -93,106 +103,122 @@ fn draw_selection_screen(
                 .body(|mut body| {
                     for (server, server_information) in servers.iter() {
                         body.row(20.0, |mut row| {
-                            row.col(|ui| {
-                                ui.label(&server_information.name);
-                            });
-                            row.col(|ui| {
-                                ui.label(format!("{}", server_information.address));
-                            });
-                            row.col(|ui| {
-                                use egui::Color32;
-                                use networking::{Empirical::*, SecurityLevel::*};
-                                match server_information.security {
-                                    Confirmed(AuthoritySigned) => {
-                                        ui.colored_label(Color32::GREEN, "\u{2713} (CA-signed)")
-                                    }
-                                    Confirmed(SelfSigned) => {
-                                        ui.colored_label(Color32::YELLOW, "\u{2713} (self-signed)")
-                                    }
-                                    Confirmed(NoVerification) => {
-                                        ui.colored_label(Color32::RED, "\u{2713} (unsigned)")
-                                    }
-                                    Confirmed(ConnectionFailed) => {
-                                        ui.colored_label(Color32::LIGHT_GRAY, "\u{2717} (failed)")
-                                    }
-                                    Unconfirmed(AuthoritySigned) => {
-                                        ui.colored_label(Color32::GREEN, "...\u{1F4DE} (CA-signed)")
-                                    }
-                                    Unconfirmed(SelfSigned) => ui.colored_label(
-                                        Color32::YELLOW,
-                                        "...\u{1F4DE} (self-signed)",
-                                    ),
-                                    Unconfirmed(NoVerification) => {
-                                        ui.colored_label(Color32::RED, "...\u{1F4DE} (unsigned)")
-                                    }
-                                    Unconfirmed(ConnectionFailed) => {
-                                        ui.colored_label(Color32::LIGHT_GRAY, "\u{2717} (failed)")
-                                    }
-                                };
-                            });
-                            row.col(|ui| {
-                                let mut enabled = true;
-                                if let networking::Empirical::Unconfirmed(_) =
-                                    server_information.security
-                                {
-                                    enabled = false;
-                                } else if let networking::Empirical::Confirmed(
-                                    networking::SecurityLevel::ConnectionFailed,
-                                ) = server_information.security
-                                {
-                                    enabled = false;
-                                }
-                                let join_button =
-                                    ui.add_enabled(enabled, egui::Button::new("Join"));
-                                if join_button.clicked() {
-                                    commands.insert_resource(networking::CurrentServer(server));
-                                    commands.insert_resource(NextState(GameState::Joining));
-                                }
-                            });
+                            draw_server_list_row(
+                                &mut row,
+                                server,
+                                server_information,
+                                &mut commands,
+                            );
                         });
                     }
                 });
+
             ui.separator();
-            ui.label("Add other server:");
-            let socket_address = &mut ui_state.server_address;
-            let address_text_edit = ui.text_edit_singleline(socket_address);
-            if state.is_changed() {
-                address_text_edit.request_focus();
-            }
-            let add_server_button = ui.button("Add");
-            let popup_id = ui.make_persistent_id("add_button_popup");
-            let confirmed_with_keyboard =
-                address_text_edit.lost_focus() && keyboard.pressed(KeyCode::Return);
-            if add_server_button.clicked() || confirmed_with_keyboard {
-                match networking::ServerInformation::from_str(socket_address) {
-                    Ok(server_information) => {
-                        if !servers.iter().any(|(_, other_server_information)| {
-                            server_information.address == other_server_information.address
-                        }) {
-                            // Only add server if it does not exist already.
-                            let entity = commands.spawn(server_information.clone()).id();
-                            server_information.connect(&mut commands, entity, &mut client);
+            ui.horizontal(|ui| {
+                ui.label("Add other server:");
+                let socket_address = &mut ui_state.server_address;
+                let address_text_edit = ui.text_edit_singleline(socket_address);
+                if state.is_changed() {
+                    address_text_edit.request_focus();
+                }
+                let add_server_button = ui.button("Add");
+                let popup_id = ui.make_persistent_id("add_button_popup");
+                let confirmed_with_keyboard =
+                    address_text_edit.lost_focus() && keyboard.pressed(KeyCode::Return);
+                if add_server_button.clicked() || confirmed_with_keyboard {
+                    match networking::ServerInformation::from_str(socket_address) {
+                        Ok(server_information) => {
+                            if !servers.iter().any(|(_, other_server_information)| {
+                                server_information.address == other_server_information.address
+                            }) {
+                                // Only add server if it does not exist already.
+                                let entity = commands.spawn(server_information.clone()).id();
+                                server_information.connect(&mut commands, entity, &mut client);
+                            }
+                        }
+                        Err(error) => {
+                            ui_state.error_message = error;
+                            ui.memory().toggle_popup(popup_id);
                         }
                     }
-                    Err(error) => {
-                        ui_state.error_message = error;
-                        ui.memory().toggle_popup(popup_id);
-                    }
                 }
-            }
 
-            let above = egui::AboveOrBelow::Above;
-            egui::popup::popup_above_or_below_widget(
-                ui,
-                popup_id,
-                &add_server_button,
-                above,
-                |ui| {
-                    ui.set_min_width(200.0);
-                    ui.label(ui_state.error_message.clone());
-                },
-            );
+                let above = egui::AboveOrBelow::Above;
+                egui::popup::popup_above_or_below_widget(
+                    ui,
+                    popup_id,
+                    &add_server_button,
+                    above,
+                    |ui| {
+                        ui.set_min_width(200.0);
+                        ui.label(ui_state.error_message.clone());
+                    },
+                );
+            });
+
+            ui.separator();
+            ui.horizontal(|ui| {
+                ui.label("User name:           ");
+                let name_text_edit = ui.text_edit_singleline(&mut ui_state.user_name);
+                if name_text_edit.lost_focus() {
+                    commands.insert_resource(lobby::UserName(ui_state.user_name.clone()));
+                }
+            });
         });
+    });
+}
+
+fn draw_server_list_row(
+    row: &mut egui_extras::TableRow,
+    server: Entity,
+    server_information: &networking::ServerInformation,
+    commands: &mut Commands,
+) {
+    row.col(|ui| {
+        ui.label(&server_information.name);
+    });
+    row.col(|ui| {
+        ui.label(format!("{}", server_information.address));
+    });
+    row.col(|ui| {
+        use egui::Color32;
+        use networking::{Empirical::*, SecurityLevel::*};
+        match server_information.security {
+            Confirmed(AuthoritySigned) => ui.colored_label(Color32::GREEN, "\u{2713} (CA-signed)"),
+            Confirmed(SelfSigned) => ui.colored_label(Color32::YELLOW, "\u{2713} (self-signed)"),
+            Confirmed(NoVerification) => ui.colored_label(Color32::RED, "\u{2713} (unsigned)"),
+            Confirmed(ConnectionFailed) => {
+                ui.colored_label(Color32::LIGHT_GRAY, "\u{2717} (failed)")
+            }
+            Unconfirmed(AuthoritySigned) => {
+                ui.colored_label(Color32::GREEN, "...\u{1F4DE} (CA-signed)")
+            }
+            Unconfirmed(SelfSigned) => {
+                ui.colored_label(Color32::YELLOW, "...\u{1F4DE} (self-signed)")
+            }
+            Unconfirmed(NoVerification) => {
+                ui.colored_label(Color32::RED, "...\u{1F4DE} (unsigned)")
+            }
+            Unconfirmed(ConnectionFailed) => {
+                ui.colored_label(Color32::LIGHT_GRAY, "\u{2717} (failed)")
+            }
+        };
+    });
+    row.col(|ui| {
+        let mut enabled = true;
+        if let networking::Empirical::Unconfirmed(_) = server_information.security {
+            enabled = false;
+        } else if let networking::Empirical::Confirmed(
+            networking::SecurityLevel::ConnectionFailed,
+        ) = server_information.security
+        {
+            enabled = false;
+        }
+        let join_button = ui.add_enabled(enabled, egui::Button::new("Join"));
+        if join_button.clicked() {
+            commands.insert_resource(networking::CurrentServer(server));
+            commands.insert_resource(NextState(GameState::Joining));
+        }
     });
 }
 
