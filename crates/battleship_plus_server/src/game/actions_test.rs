0a81b223788d6d778f6c6f699c53385de6d5efb5
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2023-01-13 16:03:04 +0100

    allow a ship number only once per player + test

--- crates/battleship_plus_server/src/game/actions_test.rs
+++ crates/battleship_plus_server/src/game/actions_test.rs
@@ -1378,7 +1378,7 @@ mod actions_place_ships {
     use crate::game::states::GameState;
 
     #[tokio::test]
-    async fn actions_place_ship() {
+    async fn actions_place_ships() {
         let player = Player::default();
 
         let g = Arc::new(RwLock::new(Game {
@@ -1457,7 +1457,7 @@ mod actions_place_ships {
     }
 
     #[tokio::test]
-    async fn actions_place_ship_outside_quadrant() {
+    async fn actions_place_ships_outside_quadrant() {
         let player = Player::default();
 
         let g = Arc::new(RwLock::new(Game {
@@ -1533,7 +1533,7 @@ mod actions_place_ships {
     }
 
     #[tokio::test]
-    async fn actions_place_ship_wrong_ship_set_missing_ships() {
+    async fn actions_place_ships_wrong_ship_set_missing_ships() {
         let player = Player::default();
 
         let g = Arc::new(RwLock::new(Game {
@@ -1611,7 +1611,7 @@ mod actions_place_ships {
     }
 
     #[tokio::test]
-    async fn actions_place_ship_wrong_ship_set_too_many_ships() {
+    async fn actions_place_ships_wrong_ship_set_too_many_ships() {
         let player = Player::default();
 
         let g = Arc::new(RwLock::new(Game {
@@ -1697,7 +1697,7 @@ mod actions_place_ships {
     }
 
     #[tokio::test]
-    async fn actions_place_ship_colliding() {
+    async fn actions_place_ships_colliding() {
         let player = Player::default();
 
         let g = Arc::new(RwLock::new(Game {
@@ -1756,6 +1756,84 @@ mod actions_place_ships {
             })
             .collect();
         ship_assignments.shuffle(&mut thread_rng());
+
+        // place ships
+        assert!(Action::PlaceShips {
+            player_id: player.id,
+            ship_placements: ship_assignments,
+        }
+        .apply_on(&mut g)
+        .is_err());
+
+        // check game
+        ships_to_be_placed.iter().for_each(|ship_expected| {
+            let ship_actual = g.ships.get_by_id(&ship_expected.id());
+            assert!(ship_actual.is_none());
+        })
+    }
+
+    #[tokio::test]
+    async fn actions_place_ships_one_ship_number_multiple_times() {
+        let player = Player::default();
+
+        let g = Arc::new(RwLock::new(Game {
+            players: HashMap::from([(player.id, player.clone())]),
+            team_a: HashSet::from([player.id]),
+            ..Default::default()
+        }));
+        let mut g = g.write().await;
+        g.state = GameState::Preparation;
+        g.players.get_mut(&player.id).unwrap().quadrant = g.quadrants().first().cloned();
+        let player = g.players.get(&player.id).unwrap().clone();
+
+        let ship_positions_orientation = vec![
+            (0, 0, Orientation::East),  // Carrier
+            (0, 1, Orientation::East),  // Battleship
+            (0, 2, Orientation::East),  // Battleship
+            (0, 3, Orientation::East),  // Cruiser
+            (0, 4, Orientation::East),  // Cruiser
+            (0, 5, Orientation::East),  // Cruiser
+            (0, 6, Orientation::East),  // Submarine
+            (0, 7, Orientation::East),  // Submarine
+            (0, 8, Orientation::East),  // Submarine
+            (0, 9, Orientation::East),  // Submarine
+            (0, 10, Orientation::East), // Destroyer
+            (0, 11, Orientation::East), // Destroyer
+        ];
+
+        let ships_to_be_placed: Vec<_> = g
+            .config
+            .ship_set_team_a
+            .iter()
+            .enumerate()
+            .map(|(ship_number, &ship_id)| (ship_number, ShipType::from_i32(ship_id).unwrap()))
+            .zip(ship_positions_orientation)
+            .map(|((ship_number, ship_type), (x, y, orientation))| {
+                Ship::new_from_type(
+                    ship_type,
+                    (player.id, ship_number as u32),
+                    (x, y),
+                    orientation,
+                    g.config.clone(),
+                )
+            })
+            .collect();
+
+        let mut ship_assignments: Vec<_> = ships_to_be_placed
+            .iter()
+            .enumerate()
+            .map(|(i, ship)| ShipAssignment {
+                ship_number: i as u32,
+                coordinate: Some(Coordinate {
+                    x: ship.position().0 as u32,
+                    y: ship.position().1 as u32,
+                }),
+                direction: <Orientation as Into<Direction>>::into(ship.orientation()) as i32,
+            })
+            .collect();
+        ship_assignments.shuffle(&mut thread_rng());
+        let ship_number = ship_assignments.first().unwrap().ship_number;
+        ship_assignments.last_mut().unwrap().ship_number = ship_number;
 
         // place ships
         assert!(Action::PlaceShips {
