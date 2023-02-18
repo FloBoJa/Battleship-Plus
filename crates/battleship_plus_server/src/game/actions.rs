7d3e8ce58d83eeaaeadd607055a735d821cf1d6f
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2023-02-18 12:37:52 +0100

    implement torpedo missile action

--- crates/battleship_plus_server/src/game/actions.rs
+++ crates/battleship_plus_server/src/game/actions.rs
@@ -288,7 +288,56 @@ impl Action {
                 }
             }
             // TODO Implementation: Action::EngineBoost { .. } => {}
-            // TODO Implementation: Action::Torpedo { .. } => {}
+            Action::Torpedo {
+                ship_id,
+                properties,
+            } => {
+                let player_id = ship_id.0;
+                check_player_exists(game, player_id)?;
+                check_players_turn(game, player_id)?;
+
+                let direction = match Direction::from_i32(properties.direction) {
+                    Some(d) => d,
+                    None => {
+                        return Err(ActionExecutionError::BadRequest(String::from(
+                            "invalid direction",
+                        )))
+                    }
+                };
+
+                match game.ships.torpedo(
+                    &mut game.turn.as_mut().unwrap().action_points_left,
+                    ship_id,
+                    direction,
+                ) {
+                    Ok((hit_ships, destroyed_ships, damage_per_hit, blast_radius)) => {
+                        Ok(Some(ActionResult {
+                            inflicted_damage_at: hit_ships
+                                .iter()
+                                .cloned()
+                                .chain(destroyed_ships.iter())
+                                .flat_map(|s| envelope_to_points(s.envelope()))
+                                .filter(|c| blast_radius.contains_point(&[c.x as i32, c.y as i32]))
+                                .collect(),
+                            inflicted_damage_by_ship: HashMap::from_iter(
+                                hit_ships
+                                    .iter()
+                                    .cloned()
+                                    .chain(destroyed_ships.iter())
+                                    .map(|ship| (ship.id(), damage_per_hit)),
+                            ),
+                            ships_destroyed: destroyed_ships.iter().map(|ship| ship.id()).collect(),
+                            gain_vision_at: HashSet::with_capacity(0),
+                            lost_vision_at: destroyed_ships
+                                .iter()
+                                .flat_map(|ship| envelope_to_points(ship.envelope()))
+                                .collect(),
+                            temp_vision_at: HashSet::with_capacity(0),
+                        }))
+                    }
+                    Err(e) => Err(ActionExecutionError::Validation(e)),
+                }
+            }
             // TODO Implementation: Action::MultiMissile { .. } => {}
             Action::None => Ok(None),
             _ => todo!(),
