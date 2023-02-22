2abfe864a5cb130577b453402d4f69bd53e3992f
Ludger Halpick <15873-uxmlz@users.noreply.git.scc.kit.edu>
2023-02-22 12:08:19 +0100

    implement multi missile compliant with rfc

--- crates/battleship_plus_common/src/game/ship_manager.rs
+++ crates/battleship_plus_common/src/game/ship_manager.rs
@@ -451,7 +451,7 @@ impl ShipManager {
         Ok(AreaOfEffect {
             hit_ships: hit_ships
                 .iter()
-                .filter_map(|id| self.ships.get(id))
+                .filter_map(|id| self.ships.get(id).cloned())
                 .collect::<Vec<_>>(),
             destroyed_ships,
             damage_per_hit: balancing.torpedo_damage,
@@ -520,7 +520,7 @@ impl ShipManager {
             });
         }
 
-        let blast_radius = AABB::from_corners(
+        let blast_area = AABB::from_corners(
             [
                 center[0] + balancing.predator_missile_radius as i32,
                 center[1] + balancing.predator_missile_radius as i32,
@@ -533,7 +533,7 @@ impl ShipManager {
 
         let hit_ships = self
             .ships_geo_lookup
-            .locate_in_envelope_intersecting(&blast_radius)
+            .locate_in_envelope_intersecting(&blast_area)
             .map(|node| node.ship_id)
             .collect::<Vec<_>>();
 
@@ -552,11 +552,11 @@ impl ShipManager {
         Ok(AreaOfEffect {
             hit_ships: hit_ships
                 .iter()
-                .filter_map(|id| self.ships.get(id))
+                .filter_map(|id| self.ships.get(id).cloned())
                 .collect::<Vec<_>>(),
             destroyed_ships,
             damage_per_hit: balancing.predator_missile_damage,
-            area: blast_radius,
+            area: blast_area,
         })
     }
 
@@ -648,7 +648,7 @@ impl ShipManager {
         bounds: &AABB<[i32; 2]>,
         ship_id: &ShipID,
         positions: Vec<Coordinate>,
-    ) -> Result<Vec<(ShotResult, Coordinate)>, ActionValidationError> {
+    ) -> Result<Vec<AreaOfEffect>, ActionValidationError> {
         if !positions
             .iter()
             .any(|p| bounds.contains_point(&[p.x as i32, p.y as i32]))
@@ -657,7 +657,6 @@ impl ShipManager {
             return Err(ActionValidationError::OutOfMap);
         }
 
-        let ship = self.ships.get(ship_id).cloned();
         let destroyer = match self.ships.get_mut(ship_id) {
             None => return Err(ActionValidationError::NonExistentShip { id: *ship_id }),
             Some(Ship::Destroyer {
@@ -667,7 +666,6 @@ impl ShipManager {
             }) => (balancing, cool_downs, data),
             _ => return Err(ActionValidationError::InvalidShipType),
         };
-        let ship = ship.unwrap();
 
         // cooldown check
         let remaining_rounds = destroyer.1.iter().find_map(|cd| match cd {
@@ -693,13 +691,6 @@ impl ShipManager {
             });
         }
 
-        // check range
-        if positions.iter().any(|p| {
-            ship.distance_2(&[p.x as i32, p.y as i32]) > balancing.multi_missile_radius as i32
-        }) {
-            return Err(ActionValidationError::Unreachable);
-        }
-
         // enforce costs
         *action_points -= costs.action_points;
         if costs.cooldown > 0 {
@@ -711,32 +702,49 @@ impl ShipManager {
         Ok(positions
             .iter()
             .map(|p| {
+                let blast_area = AABB::from_corners(
+                    [
+                        p.x as i32 - balancing.multi_missile_radius as i32,
+                        p.y as i32 - balancing.multi_missile_radius as i32,
+                    ],
+                    [
+                        p.x as i32 + balancing.multi_missile_radius as i32,
+                        p.y as i32 + balancing.multi_missile_radius as i32,
+                    ],
+                );
+
                 (
                     self.ships_geo_lookup
-                        .locate_at_point(&[p.x as i32, p.y as i32]),
-                    p,
+                        .locate_in_envelope_intersecting(&blast_area)
+                        .map(|node| node.ship_id)
+                        .collect::<HashSet<_>>(),
+                    blast_area,
                 )
             })
-            .map(|(node, p)| {
-                (
-                    if let Some(node) = node {
-                        let mut ship = self.ships.remove(&node.ship_id).unwrap();
-
-                        if ship.apply_damage(balancing.multi_missile_damage) {
-                            ShotResult::Destroyed(
-                                ship.id(),
-                                balancing.multi_missile_damage,
-                                envelope_to_points(node.envelope).collect(),
-                            )
-                        } else {
-                            self.ships.insert(node.ship_id, ship);
-                            ShotResult::Hit(node.ship_id, balancing.multi_missile_damage)
+            .map(|(hit_ships, blast_area)| {
+                let destroyed_ships = hit_ships
+                    .iter()
+                    .filter_map(|id| match self.ships.get_mut(&id) {
+                        Some(ship) => {
+                            if ship.apply_damage(balancing.multi_missile_damage) {
+                                self.ships.remove(&id)
+                            } else {
+                                None
+                            }
                         }
-                    } else {
-                        ShotResult::Miss
-                    },
-                    p.clone(),
-                )
+                        None => None,
+                    })
+                    .collect();
+
+                AreaOfEffect {
+                    hit_ships: hit_ships
+                        .iter()
+                        .filter_map(|id| self.ships.get(&id).cloned())
+                        .collect::<Vec<_>>(),
+                    destroyed_ships,
+                    damage_per_hit: balancing.multi_missile_damage,
+                    area: blast_area,
+                }
             })
             .collect::<Vec<_>>())
     }
@@ -836,8 +844,8 @@ pub fn envelope_to_points(envelope: AABB<[i32; 2]>) -> impl Iterator<Item = Coor
 }
 
 #[derive(Debug, Clone)]
-pub struct AreaOfEffect<'a> {
-    pub hit_ships: Vec<&'a Ship>,
+pub struct AreaOfEffect {
+    pub hit_ships: Vec<Ship>,
     pub destroyed_ships: Vec<Ship>,
     pub damage_per_hit: u32,
     pub area: AABB<[i32; 2]>,
--- crates/battleship_plus_server/src/game/actions_test/multi_missile.rs
+++ crates/battleship_plus_server/src/game/actions_test/multi_missile.rs
@@ -16,7 +16,7 @@ use crate::game::states::GameState;
 
 #[tokio::test]
 async fn actions_multi_missile() {
-    const MISSILE_RANGE: u32 = 40;
+    const MISSILE_RADIUS: u32 = 1;
 
     let player = Player::default();
     let ship_id = (player.id, 0);
@@ -28,33 +28,67 @@ async fn actions_multi_missile() {
         .position(0, 0)
         .orientation(Orientation::North)
         .destroyer()
-        .multi_missile(MISSILE_RANGE, 9)
+        .multi_missile(MISSILE_RADIUS, 9)
         .build();
 
     let mut ship_numbers = 0..;
 
-    let destroyed = GeneralShipBuilder::default()
+    let destroyed1 = GeneralShipBuilder::default()
         .health(5)
-        .position(2, 0)
+        .position(20, 0)
         .owner(42)
         .number(ship_numbers.next().unwrap())
         .orientation(Orientation::North)
         .destroyer()
         .build();
-    let hit = GeneralShipBuilder::default()
+    let hit1 = GeneralShipBuilder::default()
         .health(10)
-        .position(3, 0)
+        .position(21, 0)
         .owner(42)
         .number(ship_numbers.next().unwrap())
         .orientation(Orientation::North)
         .destroyer()
         .build();
-    let missed = GeneralShipBuilder::default()
+    let missed1 = GeneralShipBuilder::default()
         .health(5)
-        .position(5, 0)
+        .position(22, 0)
         .owner(42)
         .number(ship_numbers.next().unwrap())
         .orientation(Orientation::North)
+        .destroyer()
+        .build();
+
+    let destroyed2 = GeneralShipBuilder::default()
+        .health(5)
+        .position(30, 0)
+        .owner(42)
+        .number(ship_numbers.next().unwrap())
+        .orientation(Orientation::North)
+        .destroyer()
+        .build();
+    let hit2 = GeneralShipBuilder::default()
+        .health(10)
+        .position(31, 0)
+        .owner(42)
+        .number(ship_numbers.next().unwrap())
+        .orientation(Orientation::North)
+        .destroyer()
+        .build();
+    let missed2 = GeneralShipBuilder::default()
+        .health(5)
+        .position(32, 0)
+        .owner(42)
+        .number(ship_numbers.next().unwrap())
+        .orientation(Orientation::North)
+        .destroyer()
+        .build();
+
+    let destroyed_overlapping = GeneralShipBuilder::default()
+        .health(10)
+        .position(29, 0)
+        .owner(42)
+        .number(ship_numbers.next().unwrap())
+        .orientation(Orientation::West)
         .destroyer()
         .build();
 
@@ -67,9 +101,13 @@ async fn actions_multi_missile() {
         players: HashMap::from([(player.id, player.clone())]),
         ships: ShipManager::new_with_ships(vec![
             ship.clone(),
-            destroyed.clone(),
-            hit.clone(),
-            missed.clone(),
+            destroyed1.clone(),
+            hit1.clone(),
+            missed1.clone(),
+            destroyed2.clone(),
+            hit2.clone(),
+            missed2.clone(),
+            destroyed_overlapping.clone(),
         ]),
         team_a: HashSet::from([player.id]),
         ..Default::default()
@@ -79,9 +117,9 @@ async fn actions_multi_missile() {
     let result = MultiMissile {
         ship_id,
         properties: MultiMissileProperties {
-            position_a: Some(Coordinate { x: 2, y: 0 }),
-            position_b: Some(Coordinate { x: 3, y: 0 }),
-            position_c: Some(Coordinate { x: 4, y: 0 }),
+            position_a: Some(Coordinate { x: 20, y: 0 }),
+            position_b: Some(Coordinate { x: 30, y: 0 }),
+            position_c: Some(Coordinate { x: 27, y: 0 }),
         },
     }
     .apply_on(&mut g);
@@ -95,20 +133,37 @@ async fn actions_multi_missile() {
         temp_vision_at,
     })) = result
     {
-        assert_eq!(inflicted_damage_by_ship.len(), 2);
-        assert!(inflicted_damage_by_ship.contains_key(&destroyed.id()));
-        assert!(inflicted_damage_by_ship.contains_key(&hit.id()));
+        assert!(inflicted_damage_by_ship.contains_key(&destroyed1.id()));
+        assert!(inflicted_damage_by_ship.contains_key(&hit1.id()));
+        assert!(inflicted_damage_by_ship.contains_key(&destroyed2.id()));
+        assert!(inflicted_damage_by_ship.contains_key(&hit2.id()));
+        assert!(inflicted_damage_by_ship.contains_key(&destroyed_overlapping.id()));
+        assert_eq!(inflicted_damage_by_ship.len(), 5);
 
-        assert_eq!(inflicted_damage_at.len(), 2);
-        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 2, y: 0 }));
-        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 3, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 20, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 20, y: 1 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 21, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 21, y: 1 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 30, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 30, y: 1 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 31, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 31, y: 1 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 28, y: 0 }));
+        assert!(inflicted_damage_at.contains_key(&Coordinate { x: 29, y: 0 }));
+        assert_eq!(inflicted_damage_at.len(), 10);
 
-        assert_eq!(ships_destroyed.len(), 1);
-        assert!(ships_destroyed.contains(&destroyed.id()));
+        assert!(ships_destroyed.contains(&destroyed1.id()));
+        assert!(ships_destroyed.contains(&destroyed2.id()));
+        assert!(ships_destroyed.contains(&destroyed_overlapping.id()));
+        assert_eq!(ships_destroyed.len(), 3);
 
-        assert_eq!(lost_vision_at.len(), 2);
-        assert!(lost_vision_at.contains(&Coordinate { x: 2, y: 0 }));
-        assert!(lost_vision_at.contains(&Coordinate { x: 2, y: 1 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 20, y: 0 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 20, y: 1 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 30, y: 0 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 30, y: 1 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 29, y: 0 }));
+        assert!(lost_vision_at.contains(&Coordinate { x: 28, y: 0 }));
+        assert_eq!(lost_vision_at.len(), 6);
 
         assert!(gain_vision_at.is_empty());
         assert!(temp_vision_at.is_empty());
@@ -121,16 +176,22 @@ async fn actions_multi_missile() {
         remaining_rounds: 10
     }));
 
-    assert_eq!(g.ships.get_by_id(&destroyed.id()), None);
-    assert!(g.ships.get_by_id(&hit.id()).is_some());
-    assert!(g.ships.get_by_id(&missed.id()).is_some());
-    assert_eq!(g.ships.get_by_id(&hit.id()).unwrap().data().health, 1);
-    assert_eq!(g.ships.get_by_id(&missed.id()).unwrap().data().health, 5);
+    assert_eq!(g.ships.get_by_id(&destroyed1.id()), None);
+    assert_eq!(g.ships.get_by_id(&destroyed2.id()), None);
+    assert_eq!(g.ships.get_by_id(&destroyed_overlapping.id()), None);
+    assert!(g.ships.get_by_id(&hit1.id()).is_some());
+    assert!(g.ships.get_by_id(&hit2.id()).is_some());
+    assert!(g.ships.get_by_id(&missed1.id()).is_some());
+    assert!(g.ships.get_by_id(&missed2.id()).is_some());
+    assert_eq!(g.ships.get_by_id(&hit1.id()).unwrap().data().health, 1);
+    assert_eq!(g.ships.get_by_id(&hit2.id()).unwrap().data().health, 1);
+    assert_eq!(g.ships.get_by_id(&missed1.id()).unwrap().data().health, 5);
+    assert_eq!(g.ships.get_by_id(&missed2.id()).unwrap().data().health, 5);
 }
 
 #[tokio::test]
 async fn actions_multi_missile_same_spot() {
-    const MISSILE_RANGE: u32 = 40;
+    const MISSILE_RADIUS: u32 = 0;
 
     let player = Player::default();
     let ship_id = (player.id, 0);
@@ -142,7 +203,7 @@ async fn actions_multi_missile_same_spot() {
         .position(0, 0)
         .orientation(Orientation::North)
         .destroyer()
-        .multi_missile(MISSILE_RANGE, 10)
+        .multi_missile(MISSILE_RADIUS, 10)
         .build();
 
     let mut ship_numbers = 0..;
